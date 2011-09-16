ebc69996662e04065b0117c300fc2b7f1aed46fa
James Griffin <jsgriffin@imaginaryroots.co.uk>
2011-09-16 21:34:08 +0100

    More styling

--- libs/frontend/public/js/scripts.js
+++ libs/frontend/public/js/scripts.js
@@ -4,4 +4,19 @@ $(function(){
     dashboard = new Dashboard();
     dashboard.fetchWidgetTemplate('test');
     dashboard.convertWidgetNameToClassName('test-beans-1-dogs');
+    
+    $('#widgets-panel').animate({
+        height: '15px'
+    });
+    
+    $('#widgets-panel .show a').click(function() {
+        if($('#widgets-panel').height() < 150)  
+            height = '150px'; 
+        else 
+            height = '15px';
+                
+        $('#widgets-panel').animate({
+            height: height
+        });
+    });
 });
